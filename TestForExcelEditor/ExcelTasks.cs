51b47bb0315171be36cc26e732c1b11936da4241
Reading Steiner <ousama.askri01@gmail.com>
2024-11-17 13:42:07 +0100

    einfügen von dummy test uzm zu schauen ob e sfunktioniert mit automatisierten tests.

--- TestForExcelEditor/ExcelTasks.cs
+++ TestForExcelEditor/ExcelTasks.cs
@@ -5,24 +5,37 @@ namespace UnitTests
     [TestFixture]
     public class Tests
     {
+        //[Test]
+        //public void TestGetDefinedCellsValues()
+        //{
+        //    // Arrange
+        //    string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Testfiles\TestFile.xlsx");
+
+        //    List<Mapping> definedCells = new List<Mapping>{
+        //        new Mapping("TextFieldA", "A1"),
+        //        new Mapping("TextFieldB", "D27"),
+        //        new Mapping("TextFieldC", "I16"),
+        //        new Mapping("TextFieldD", "M23")
+        //    };
+
+        //    // Act
+        //    string result = ExcelEditor.Helper.GetDefinedCellsValues(filePath, definedCells);
+
+        //    // Assert
+        //    Assert.AreEqual(result, "A1: Press ==> TextFieldA\r\nD27: Parallel ==> TextFieldB\r\nI16: Round ==> TextFieldC\r\nM23: 50000 ==> TextFieldD\r\n");
+        //}
+
         [Test]
-        public void TestGetDefinedCellsValues()
+        public void TestFoo()
         {
             // Arrange
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Testfiles\TestFile.xlsx");
-
-            List<Mapping> definedCells = new List<Mapping>{
-                new Mapping("TextFieldA", "A1"),
-                new Mapping("TextFieldB", "D27"),
-                new Mapping("TextFieldC", "I16"),
-                new Mapping("TextFieldD", "M23")
-            };
+            int val = 4;
 
             // Act
-            string result = ExcelEditor.Helper.GetDefinedCellsValues(filePath, definedCells);
+            int result = val + 5;
 
             // Assert
-            Assert.AreEqual(result, "A1: Press ==> TextFieldA\r\nD27: Parallel ==> TextFieldB\r\nI16: Round ==> TextFieldC\r\nM23: 50000 ==> TextFieldD\r\n");
+            Assert.AreEqual(result, 9);
         }
     }
 }
